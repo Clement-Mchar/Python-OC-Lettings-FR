f1cb14daee155beb1e54f80cfa437992dc9922bf
Clement-Mchar <clement.mchar@gmail.com>
2024-07-30 11:04:19 +0200

    refactoring lettings app, features, paths and templates

--- lettings/urls.py
+++ lettings/urls.py
@@ -1,10 +1,9 @@
-from django.contrib import admin
 from django.urls import path
 
 from . import views
 
+app_name = "lettings"
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('lettings/', views.lettings_index, name='lettings_index'),
-    path('lettings/<int:letting_id>/', views.letting, name='letting')
+    path("", views.index, name="index"),
+    path("<int:letting_id>/", views.letting, name="letting"),
 ]
