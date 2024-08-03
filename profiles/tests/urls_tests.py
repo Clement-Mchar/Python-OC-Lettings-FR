65dd0a181ed185ef8cfa5a9bb34a8a27773f3600
Clement-Mchar <clement.mchar@gmail.com>
2024-08-03 06:07:57 +0200

    added urls testing for profiles app

---
+++ profiles/tests/urls_tests.py
@@ -0,0 +1,17 @@
+from django.urls import reverse, resolve
+from oc_lettings_site.tests.base_tests import BaseTestSetup
+
+class UrlTests(BaseTestSetup):
+
+    def test_index_url(self):
+        path = reverse('profiles:index')
+
+        assert path == "/profiles/"
+        assert resolve(path).view_name == "profiles:index"
+
+    def test_profile_url(self):
+
+        path = reverse('profiles:profile', kwargs={'username':self.test_profile.user.username})
+
+        assert path == "/profiles/JD/"
+        assert resolve(path).view_name == "profiles:profile"
