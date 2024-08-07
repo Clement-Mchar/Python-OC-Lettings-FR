e1e1cc8686a615c44ced0356bf6b343cfd8b0f24
Clement-Mchar <clement.mchar@gmail.com>
2024-08-07 17:47:37 +0200

    generated coverage report, init sentry

--- manage.py
+++ manage.py
@@ -1,6 +1,9 @@
 import os
 import sys
+import sentry_sdk
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oc_lettings_site.settings')
@@ -14,6 +17,17 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
+dsn = os.environ.get("DSN")
 
 if __name__ == '__main__':
+    sentry_sdk.init(
+        dsn=dsn,
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        traces_sample_rate=1.0,
+        # Set profiles_sample_rate to 1.0 to profile 100%
+        # of sampled transactions.
+        # We recommend adjusting this value in production.
+        profiles_sample_rate=1.0,
+    )
     main()
