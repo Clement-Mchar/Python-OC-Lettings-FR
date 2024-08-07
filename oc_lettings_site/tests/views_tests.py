06879d61e67cf9e1073bbfca2967caba80cdf035
Clement-Mchar <clement.mchar@gmail.com>
2024-08-07 04:00:04 +0200

    added view testing for index and error pages

---
+++ oc_lettings_site/tests/views_tests.py
@@ -0,0 +1,29 @@
+from django.urls import reverse, resolve
+from .base_tests import BaseTestSetup 
+
+class BaseViews(BaseTestSetup):
+
+    def test_index_view(self):
+        path = reverse('index')
+
+        response = self.client.get(path)
+
+        content = response.content.decode()
+        expected_content = '<h1 class="page-header-ui-title mb-3 display-6">Welcome to Holiday Homes</h1>'
+
+        self.assertInHTML(expected_content, content)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "index.html")
+    
+    def test_404_view(self):
+        path = '/notvalidpath'
+
+        response = self.client.get(path)
+
+        content = response.content.decode()
+        expected_content = "<p>The page you are looking for doesn't exists.</p>"
+
+        self.assertInHTML(expected_content, content)
+        self.assertEqual(response.status_code, 404)
+        self.assertTemplateUsed(response, '404.html')
+    
