a06b66b20ed7d12b409f6755a23b69168363dd3b
Clement-Mchar <clement.mchar@gmail.com>
2024-08-07 04:00:31 +0200

    added view testing for profiles app

---
+++ profiles/tests/views_tests.py
@@ -0,0 +1,26 @@
+from django.urls import reverse, resolve
+from oc_lettings_site.tests.base_tests import BaseTestSetup
+
+class ProfilesViewsTests(BaseTestSetup):
+
+    def test_profiles_index_view(self):
+        path = reverse('profiles:index')
+
+        response = self.client.get(path)
+
+        content = response.content.decode()
+        expected_content = "JD"
+
+        self.assertInHTML(expected_content, content)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "profiles/index.html")
+    
+    def test_profile_view(self):
+        path = reverse('profiles:profile', kwargs={'username':'JD'})
+        response = self.client.get(path)
+
+        content = response.content.decode()
+        expected_content = '<p><strong>Email :</strong> johndoe@email.com</p>'
+        self.assertInHTML(expected_content, content)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "profiles/profile.html")
