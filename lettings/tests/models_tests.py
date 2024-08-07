79bf23accc5f3976cf0343aba1489736f40af09e
Clement-Mchar <clement.mchar@gmail.com>
2024-08-07 04:01:06 +0200

    added model testing for address and linting in lettings app

---
+++ lettings/tests/models_tests.py
@@ -0,0 +1,15 @@
+from django.urls import reverse, resolve
+from oc_lettings_site.tests.base_tests import BaseTestSetup
+
+
+class LettingsModelsTests(BaseTestSetup):
+
+    def test_address_model(self):
+        expected_value = "1 test street"
+
+        assert str(self.test_address) == expected_value
+    
+    def test_letting_model(self):
+        expected_value = "test_letting"
+
+        assert str(self.test_letting) == expected_value
