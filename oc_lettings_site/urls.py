b1cc93a0c4e75bf18d0807c1c4ddde3488cd7905
Clement-Mchar <clement.mchar@gmail.com>
2024-08-14 17:33:57 +0200

    added staticfiles path

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from . import views
+import settings
+from django.views.static import serve
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("profiles/", include("profiles.urls", namespace="profiles")),
     path("lettings/", include("lettings.urls", namespace="lettings")),
     path("admin/", admin.site.urls),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
 ]
 
 handler404 = "oc_lettings_site.views.page_not_found"
