e1e0d7496f9d53adff5bbde8c6af4ce243e6d631
Clement-Mchar <clement.mchar@gmail.com>
2024-08-29 22:00:52 +0200

    forgot to turn debug to true

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -16,7 +16,7 @@ secret_key = os.environ.get("SECRET_KEY")
 SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".localhost", "127.0.0.1", "[::1]", "python-oc-lettings-fr-9zjh.onrender.com"]
 
